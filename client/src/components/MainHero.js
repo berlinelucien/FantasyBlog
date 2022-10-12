802a164b7530595af8eec3c7800e7ac22c94fe98
Berlin <Berline_lucien@yahoo.com>
2022-10-12 12:36:00 -0400

    material ui masonary layout

--- client/src/components/MainHero.js
+++ client/src/components/MainHero.js
@@ -1,64 +1,74 @@
-import React from 'react'
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Link from '@mui/material/Link';
-import Box from '@mui/material/Box';
-import dreamShip from "../images/dreamShip.jpg"
-
+import React from "react";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import dreamShip from "../images/dreamShip.jpg";
 
 const MainHero = () => {
-    return (
-        <Paper
+  return (
+    <Paper
+      sx={{
+        position: "relative",
+        backgroundColor: "grey.800",
+        color: "#fff",
+        mb: 5,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundImage: `url(${dreamShip})`,
+      }}
+    >
+      {/* Increase the priority of the hero background image */}
+      {<img style={{ display: "none" }} src={dreamShip} alt="" />}
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          backgroundColor: "rgba(0,0,0,.3)",
+        }}
+      />
+      <Grid container>
+        <Grid item md={6}>
+          <Box
             sx={{
-                position: 'relative',
-                backgroundColor: 'grey.800',
-                color: '#fff',
-                mb: 4,
-                backgroundSize: 'cover',
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center',
-                backgroundImage: `url(${dreamShip})`,
+              position: "relative",
+              p: { xs: 3, md: 6 },
+              pr: { md: 0 },
             }}
-        >
-            {/* Increase the priority of the hero background image */}
-            {<img style={{ display: 'none' }} src={dreamShip} alt="" />}
-            <Box
-                sx={{
-                    position: 'absolute',
-                    top: 0,
-                    bottom: 0,
-                    right: 0,
-                    left: 0,
-                    backgroundColor: 'rgba(0,0,0,.3)',
-                }}
-            />
-            <Grid container>
-                <Grid item md={6}>
-                    <Box
-                        sx={{
-                            position: 'relative',
-                            p: { xs: 3, md: 6 },
-                            pr: { md: 0 },
-                        }}
-                    >
-                        <Typography component="h1" variant="h3" color="inherit" gutterBottom>
-                            TITLE OF A LONGER FEATURED BLOG backgroundPosition
-                            {/* {post.title} */}
-                        </Typography>
-                        <Typography variant="h5" color="inherit" paragraph>
-                        Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents
-                            {/* {post.description} */}
-                        </Typography>
-                        <Link variant="subtitle1" href="#">
-                            link here if you want
-                            {/* {post.linkText} */}
-                        </Link>
-                    </Box>
-                </Grid>
-            </Grid>
-        </Paper> 
-);
-}
+          >
+            <Typography
+              component="h1"
+              variant="h3"
+              color="inherit"
+              gutterBottom
+            >
+              TITLE
+              {/* {post.title} */}
+            </Typography>
+            <Typography variant="h5" color="inherit" paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              {/* {post.description} */}
+            </Typography>
+            <Link variant="subtitle1" href="#">
+              link here if you want
+              {/* {post.linkText} */}
+            </Link>
+          </Box>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
 
 export default MainHero;
